16895149f54e73ce8bb00b700bafa20727fcb98c
38831988@qq.com <38831988@qq.com>
2018-04-18 16:28:41 +0800

    mesr162

--- src/main/java/com/facetuis/server/service/banner/BannerService.java
+++ src/main/java/com/facetuis/server/service/banner/BannerService.java
@@ -18,26 +18,39 @@ public class BannerService {
     public Page<Banner> getBanner(){
         Banner b1 = new Banner();
         b1.setUuid("1");
-        b1.setImgUrl("http://" + serverIp + "/1.jpg");
+        b1.setImgUrl("http://" + serverIp + "/banner/1.jpg");
         b1.setDescription("ddd");
         b1.setName("111");
 
         Banner b2 = new Banner();
-        b2.setUuid("1");
-        b1.setImgUrl("http://" + serverIp + "/1.jpg");
+        b2.setUuid("3");
+        b1.setImgUrl("http://" + serverIp + "/banner/2.jpg");
         b2.setDescription("ddd");
         b2.setName("111");
 
         Banner b3 = new Banner();
-        b3.setUuid("1");
-        b3.setImgUrl("http://");
+        b3.setUuid("3");
+        b3.setImgUrl("http://"+serverIp+"/banner/3.jpg");
         b3.setDescription("ddd");
         b3.setName("111");
 
+        Banner b4 = new Banner();
+        b4.setUuid("4");
+        b4.setImgUrl("http://"+serverIp+"/banner/4.jpg");
+        b4.setDescription("ddd");
+        b4.setName("44444444");
+
+        Banner b5 = new Banner();
+        b5.setUuid("5");
+        b5.setImgUrl("http://"+serverIp+"/banner/5.jpg");
+        b5.setDescription("ddd");
+        b5.setName("5555");
         List<Banner> list = new ArrayList<>();
         list.add(b1);
         list.add(b2);
         list.add(b3);
+        list.add(b4);
+        list.add(b5);
         return new PageImpl<Banner>(list);
     }
 
