274591d6392ae773bddcb638285b878c444c4194
王继永 <392931818@qq.com>
2018-04-21 19:29:36 +0800

    支付模块

--- src/main/java/com/facetuis/server/app/web/UserController.java
+++ src/main/java/com/facetuis/server/app/web/UserController.java
@@ -4,11 +4,13 @@ import com.facetuis.server.app.web.basic.BaseResponse;
 import com.facetuis.server.app.web.basic.FacetuisController;
 import com.facetuis.server.app.web.response.UserRecommanderResponse;
 import com.facetuis.server.model.user.User;
+import com.facetuis.server.model.user.UserLevel;
 import com.facetuis.server.service.user.UserService;
 import com.facetuis.server.utils.NeedLogin;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -22,7 +24,7 @@ public class UserController extends FacetuisController{
      * 获取推荐自己的用户
      * @return
      */
-    @RequestMapping("/recommender")
+    @RequestMapping(value = "/recommender",method = RequestMethod.GET)
     @NeedLogin(needLogin = true)
     public BaseResponse getCommander() {
         User user = getUser();
@@ -33,6 +35,17 @@ public class UserController extends FacetuisController{
         }
         BeanUtils.copyProperties(recommander, response);
         return successResult(new UserRecommanderResponse());
+    }
+
+    @RequestMapping(value = "/upgraded",method = RequestMethod.PUT)
+    @NeedLogin(needLogin = true)
+    public BaseResponse upgraded( ){
+        User user = getUser();
+        if(user.getLevel().equals(UserLevel.LEVEL2)){
+            return setErrorResult(600,"已经是最高级");
+        }
+        return null;
+
     }
 
 
