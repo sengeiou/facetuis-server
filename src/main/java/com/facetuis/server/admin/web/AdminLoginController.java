8ec8f25f421e5ab3f6debde849182337cb1e0bd0
e <38831988@qq.com>
2018-05-07 15:12:18 +0800

    admin后台登陆

--- src/main/java/com/facetuis/server/admin/web/AdminLoginController.java
+++ src/main/java/com/facetuis/server/admin/web/AdminLoginController.java
@@ -3,6 +3,10 @@ package com.facetuis.server.admin.web;
 import com.facetuis.server.admin.web.request.LoginRequest;
 import com.facetuis.server.app.web.basic.BaseResponse;
 import com.facetuis.server.app.web.basic.FacetuisController;
+import com.facetuis.server.model.admin.AdminUsers;
+import com.facetuis.server.service.admin.AdminUsersService;
+import com.facetuis.server.service.basic.BaseResult;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,12 +17,18 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/admin/login")
 @RestController
 public class AdminLoginController extends FacetuisController {
+    @Autowired
+    private AdminUsersService adminUsersService;
 
     @RequestMapping(method = RequestMethod.POST)
     public BaseResponse login(@RequestBody LoginRequest loginRequest, BindingResult bindingResult){
 
+       BaseResult<AdminUsers> baseResult= adminUsersService.login(loginRequest.getUserName(),loginRequest.getPassword()) ;//这里调用Service里login
+        if(baseResult.hasError()){
+            return onResult(baseResult);
+        }
 
 
-        return successResult();
+        return successResult(baseResult.getResult());
     }
 }
---
+++ src/main/java/com/facetuis/server/service/admin/AdminUsersService.java
@@ -0,0 +1,57 @@
+package com.facetuis.server.service.admin;
+
+import com.facetuis.server.dao.admin.AdminUserRepository;
+import com.facetuis.server.model.admin.AdminUsers;
+import com.facetuis.server.service.basic.BaseResult;
+import com.facetuis.server.service.basic.BasicService;
+import com.facetuis.server.utils.MD5Utils;
+import com.facetuis.server.utils.RandomUtils;
+import org.apache.commons.codec.digest.Md5Crypt;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AdminUsersService extends BasicService {
+    @Autowired
+    private AdminUserRepository adminUserRepository;
+
+
+    //根据userName 查用户信息并返回用户信息
+    public BaseResult<AdminUsers> login(String user, String pass)
+    {
+        if(StringUtils.isEmpty(user) || StringUtils.isEmpty(pass) )
+        {
+            return null;
+        }
+        AdminUsers   adminUsers= adminUserRepository.findByUserName(user);
+
+        if(adminUsers == null){
+            return new BaseResult<>(500,"用户名不存在！");
+        }
+
+        String p1= MD5Utils.MD5(pass);
+        String p2=MD5Utils.MD5(p1);
+        String p3=MD5Utils.MD5(p2);
+        String p4= MD5Utils.MD5( p3+"wjymd");
+        //String p3=MD
+        if(!adminUsers.getPassword().equals(p4))
+        {
+            return new BaseResult<>(600,"用户名或密码错误！");
+
+        }
+        //生成Auto_token
+        adminUsers.setAccessToken(RandomUtils.random(64));
+        adminUserRepository.save(adminUsers);
+        return  new BaseResult<>(adminUsers );
+    }
+
+
+    public static void main(String[] args) {
+        String p1= MD5Utils.MD5("123456");
+        String p2=MD5Utils.MD5(p1);
+        String p3=MD5Utils.MD5(p2);
+        String p4= MD5Utils.MD5( p3+"wjymd");
+        System.out.println(p4);
+    }
+}
