33ac77c1b76e2e7dba44bbd95c981e7d1fd642b6
yuepengwei <1511553839@qq.com>
2018-05-22 08:29:33 +0800

    TEST

--- src/main/java/com/facetuis/server/dao/admin/AdminModelRepository.java
+++ src/main/java/com/facetuis/server/dao/admin/AdminModelRepository.java
@@ -1,7 +1,6 @@
 package com.facetuis.server.dao.admin;
 
 import com.facetuis.server.model.admin.AdminModel;
-import com.facetuis.server.model.admin.AdminUserModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface AdminModelRepository  extends JpaRepository<AdminModel,String> {
